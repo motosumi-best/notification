8bfc22b30bab36ffe0fdbeac1b3d9dbd1b509b37
金子修一郎 <kanekoshuuichirou@kanekoshuuichirou-no-MacBook-Pro.local>
2014-09-01 00:25:34 +0900

    Initial Commit

---
+++ LocalNotification/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalNotification
+//
+//  Created by 金子修一郎 on 2014/09/01.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocalNotification/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocalNotification
+//
+//  Created by 金子修一郎 on 2014/09/01.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
